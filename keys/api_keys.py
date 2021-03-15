851a78790568d7b5bd0c1857bdafd729516228f5
Alice <martin.alice.04@gmail.com>
2021-03-14 18:37:44 -0700

    ignore

--- keys/api_keys.py
+++ keys/api_keys.py
@@ -1,5 +1,3 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
+g_key =os.environ.get("GKEY")
+weather_api_key =os.environ.get("Weather_Key")
